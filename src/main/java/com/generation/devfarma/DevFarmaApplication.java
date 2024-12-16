42f4cd7df7464f6c264b03d4fe9381096de4de52
Andre Lins <andrelins2403@hotmail.com>
2024-12-16 10:19:43 -0300

    first commit

---
+++ src/main/java/com/generation/devfarma/DevFarmaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.devfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFarmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevFarmaApplication.class, args);
+	}
+
+}
